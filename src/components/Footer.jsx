11bdc068a9d098daac967fa80f5e3ae5d63aedc8
konjuable <juha.konttinen@bc.fi>
2025-10-31 13:40:46 +0200

    hrApp Step 1 valmis.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <p>
+             Copyright ©️ 2025 Juha Konttinen - WP25K. All Rights reserved.
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <>
+            <header className={styles.header}>
+                <h1>HR Management System</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
